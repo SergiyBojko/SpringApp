b42273b5ea9cbda55d7f1e26a7ae7190a77f8869
Serhii Boiko <sergiy1300@gmail.com>
2018-10-04 20:17:38 +0300

    changed db connection to use db container name

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 docker network create --driver bridge my-net
 
-docker rm --force db spring_c
+docker rm --force containerizedDatabase spring_c
 
-docker run --name db -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=user -e MYSQL_PASSWORD=123456 -e  MYSQL_DATABASE=liquibase_test -e MYSQL_ROOT_HOST=% --network my-net -d mysql:5
+docker run --name containerizedDatabase -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=user -e MYSQL_PASSWORD=123456 -e  MYSQL_DATABASE=liquibase_test --network my-net -d mysql:5
 
-./mvnw package
+./mvnw package -Dmaven.test.skip=true
 docker build -t spring_app .
 docker run --name spring_c --network my-net -p 8080:8080 -d spring_app
 
